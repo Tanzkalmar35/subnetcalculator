70e5e4ecd8fb4c682cb5ba28047ba7ae20f1b2da
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-12-14 23:45:01 +0100

    initial setup on tauri and first designs

---
+++ SubnetCal/src-tauri/src/main.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Additional Tauri command found on: https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust! And from me!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ SubnetCal/src/main.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ * An example of calling a rust function.
+ *
+ * In this example:
+ * "greet" - The name of the rust function called.
+ * "name: 'testName'" - defines the arg that the rust function takes.
+ *
+ * async function greet() {
+ *      // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+ *      await invoke("greet", {
+ *          name: "testName",
+ *      });
+ * }
+ */
